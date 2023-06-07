fbc5cf2c41682188cc6972130e61806cda6fa1f0
tschiang23 <tien23@gmail.com>
2023-06-07 12:08:17 +0800

    feat:add admin get users

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,7 +2,8 @@ const express = require('express')
 const passport = require('../../config/passport')
 const router = express.Router()
 const adminController = require('../../controllers/adminController')
-const { adminRole } = require('../../middleware/auth')
+const { adminRole, authenticated, authenticatedAdmin } = require('../../middleware/auth')
 
 router.post('/login', passport.authenticate('local', { session: false }), adminRole, adminController.login)
+router.get('/users', authenticated, authenticatedAdmin, adminController.getUsers)
 module.exports = router
